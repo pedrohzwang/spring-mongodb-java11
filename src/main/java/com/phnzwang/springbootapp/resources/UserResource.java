bec6b5d7999a74772dd9cbc9540f2d5b8f81887a
pedrohzwang <pedroh.zwang@gmail.com>
2020-10-24 17:19:17 -0300

    created repository and service and mongodb conection

--- src/main/java/com/phnzwang/springbootapp/resources/UserResource.java
+++ src/main/java/com/phnzwang/springbootapp/resources/UserResource.java
@@ -2,14 +2,13 @@ package com.phnzwang.springbootapp.resources;
 
 import ch.qos.logback.core.pattern.util.RegularEscapeUtil;
 import com.phnzwang.springbootapp.domains.User;
+import com.phnzwang.springbootapp.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
@@ -17,12 +16,12 @@ import java.util.List;
 
 public class UserResource {
 
+    @Autowired
+    private UserService service;
+
     @GetMapping
     public ResponseEntity<List<User>> findAll(){
-        User maria = new User("1", "Maria", "maria@gmail.com");
-        User max = new User("2", "Max", "max@gmail.com");
-        List<User> list = new ArrayList<>();
-        list.addAll(Arrays.asList(maria, max));
+        List<User> list = service.findAll();
         return ResponseEntity.ok().body(list);
     }
 
---
+++ src/main/java/com/phnzwang/springbootapp/services/UserService.java
@@ -0,0 +1,20 @@
+package com.phnzwang.springbootapp.services;
+
+import com.phnzwang.springbootapp.domains.User;
+import com.phnzwang.springbootapp.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repo;
+
+    public List<User> findAll(){
+        return repo.findAll();
+    }
+
+}
